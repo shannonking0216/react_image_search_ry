fed28b733ee0f3039cc2150e096487802987f627
Shannon King <kingshannon01@gmail.com>
2019-10-13 15:19:06 -0700

    added instructions and link

--- react_image_search/src/App.js
+++ react_image_search/src/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import NavBar from "./components/navbar/NavBar";
 import Search from "./components/search/Search";
@@ -11,6 +11,12 @@ class App extends Component {
       <MuiThemeProvider>
         <div>
           <NavBar />
+          <div>
+            <h1>Welcome to Shannon's PixaBay API Project</h1>
+            <h2>Choose amount of images from drop-down menu.</h2>
+            <h3>Search for images by category and press enter.</h3>
+            <h4>All images can be downloaded from: <a href="https://pixabay.com/" target="_blank">PixaBay</a></h4>
+          </div>
           <Search />
         </div>
       </MuiThemeProvider>
